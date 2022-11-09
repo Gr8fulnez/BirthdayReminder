8228e8388c6fc887d1b4c2f3fc7e6be913c3debd
New User <laptop@News-MacBook-Pro.local>
2022-11-09 10:18:00 +0100

    second commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import data from "./Data.js";
+import List from "./List.js";
+function App() {
+  return <h2>reminder project setup</h2>;
+}
+
+export default App;
